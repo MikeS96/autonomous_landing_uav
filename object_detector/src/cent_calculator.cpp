bfca1ba0cc195fb5d4caf760a55df68147d17857
Sherlock <miguel.saaruiz@gmail.com>
2020-04-30 20:13:36 -0500

    Improved cent_calculator.cpp code with a better flow and comments

--- object_detector/src/cent_calculator.cpp
+++ object_detector/src/cent_calculator.cpp
@@ -1,97 +1,154 @@
+/**
+ *  @file cent_calculator.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief This node computes the centroid, height, widhth and angle WRT to x
+ *  of a template
+ *  @version 0.1
+ *  @date 04-30-2020
+ * 
+ *  Copyright (c) 2020 Miguel Saavedra
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 #include <ros/ros.h>
 #include <find_object_2d/ObjectsStamped.h>
-#include "object_detector/Corners.h" //Custom msgs corners to recibe the corners from the corners detector node
-#include "object_detector/states.h" //Custom msgs states t publish the states to KF
-#include <QTransform>
+#include "object_detector/Corners.h" // Custom msg of type Corners
+#include "object_detector/States.h" // Custom msgs of type States
 
 # define M_PI   3.14159265358979323846  /* pi */
 
-class Position
+class States
 {
-  private: //Declaration of pubs and subs
 
-  ros::NodeHandle po_nh;
-  ros::Subscriber sub;
-  ros::Publisher pub;
+	private: 
+		// Private class attributes
+		ros::NodeHandle po_nh;
+		ros::Subscriber sub;
+		ros::Publisher pub;
 
 
-  public:
-  Position(ros::NodeHandle ao_nh) : //Constructor of the position class
-    po_nh( ao_nh )
-  {
-    pub = po_nh.advertise<object_detector::states>( "/states", 10) ; //Publisher del tipo object_detector/states que publica al topic states
-    sub = po_nh.subscribe("/corners", 10, &Position::centCalculator, this); //Suscriber del tipo object_detector/corners que se suscribe a /corners
-  }
+	public:
+		// Public class attributes and methods
+		States(ros::NodeHandle ao_nh) : po_nh( ao_nh )
+		{
+			// Publisher type object_detector::States, it publishes in /states topic
+			pub = po_nh.advertise<object_detector::States>( "/states", 10) ; 
+			// Subscriber to /corners topic from object_detector/Corners
+			sub = po_nh.subscribe("/corners", 10, &States::centCalculator, this); 
+		}
 
-  void centCalculator(const object_detector::Corners& msg) //Callback para el subscriber "CentCalculator"
-  {	
-    object_detector::states info; //Creacion del objeto tipo corners  para manejar la info
+	// Subscriber callback
+	void centCalculator(const object_detector::Corners& msg) //Callback para el subscriber "CentCalculator"
+	{	
+		// Creation of a States object to publish the info
+		object_detector::States st;
 
-	
-	//Calculo de el Angulo Theta
-	float ABx = ((msg.BottomRightX - msg.BottomLeftX)); //Calculo ancho inferior de la imagen (X)
-	float ABy = ((msg.BottomRightY - msg.BottomLeftY)); //Calculo ancho inferior de la imagen (Y)
-	float theta = atan2(ABy, ABx)*180/M_PI; //Calculo de Theta con Atan2 y conversion a grados
+		// Theta angle calculation
+		float theta = computeTheta(msg.BottomLeftX, msg.BottomLeftY,
+									msg.BottomRightX, msg.BottomRightY);
+		// Bottom vector magnitude
+		float v_bot = computeEucDist(msg.BottomLeftX, msg.BottomLeftY,
+									msg.BottomRightX, msg.BottomRightY);
+		// Right vector magnitude
+		float v_right = computeEucDist(msg.BottomRightX, msg.BottomRightY,
+									msg.TopRightX, msg.TopRightY);
+		// Top vector magnitude
+		float v_top = computeEucDist(msg.TopLeftX, msg.TopLeftY,
+									msg.TopRightX, msg.TopRightY);
+		// Left vector magnitude
+		float v_left = computeEucDist(msg.TopLeftX, msg.TopLeftY,
+									msg.BottomLeftX, msg.BottomLeftY);
 
-	float vx = ABx; //Vector entre C y D para W (Inferior)
-	float vy = ABy;
-	float v1 = sqrt(abs(vx*vx)+abs(vy*vy)); //Calcula de la magnitud neta del vector de la esquina inferior
+		// Centroid of the template in the image
+		st.Xc = msg.CenterX;
+		st.Yc = msg.CenterY;
+		// Computation of width and height as an average of the bottom and top magnitudes
+		st.W = (v_bot + v_top) / 2; 
+		st.H = (v_right + v_left) / 2;
+		st.Theta = theta;
 
-	vx = msg.TopRightX - msg.BottomRightX;  //Vector entre B y D para H (Izquierda)
-	vy = msg.TopRightY - msg.BottomRightY;
-	float v2 = sqrt(abs(vx*vx)+abs(vy*vy)); //Calcula de la magnitud neta del vector de la esquina izquierdac
-			
-	vx = msg.TopRightX - msg.TopLeftX; //Vector entre A y C para W (Superior)
-	vy = msg.TopRightY - msg.TopLeftY; 
-	float v3 = sqrt(abs(vx*vx)+abs(vy*vy)); //Calcula de la magnitud neta del vector de la esquina superior
+		// Correction of theta when the angle is bigger than 90 degrees to avoid bad orientation for the controller
+		if(theta > 90) 
+		{
+			int cont = theta / 90;
+			st.Theta = theta - cont * 90;
+		}
+		else if(theta < 90)
+		{
+			int cont = -1 * theta / 90;
+			st.Theta = theta + cont * 90;
+		}
 
-	vx = msg.BottomLeftX - msg.TopLeftX;  //Vector entre C y A para H (Inferior)
-	vy = msg.BottomLeftY - msg.TopLeftY;
-	float v4 = sqrt(abs(vx*vx)+abs(vy*vy)); //Calcula de la magnitud neta del vector de la esquina inferior
-		
-
-	//Asignacion de los datos al custom msg para ser publicado
-	info.Xc = msg.CenterX;
-	info.Yc = msg.CenterY;
-	//Caculo centroide basado en el escalamiento realizado por la homografia
-	info.W = (v1+v3)/2; //Promedio realizado entre los vectores calculador para computar Width
-	info.H = (v2+v4)/2; //Promedio realizado entre los vectores calculador para computar Height
-	info.Theta = theta;
-
-	if(theta>90) //Corecion de theta cuando el angulo sea mayor a 90 grados, para no generar problema en los datos publicados
-	{	int cont = theta/90;
-		info.Theta = theta-cont*90;
+		// Uncomment these lines to print the results on console
+		/*printf("Centroid in pix position (%f,%f)\n Object Width (%f)\n Object Height (%f)\n Theta (%f)\n", //Impresion en consola de los datos de posicion
+						
+				st.Xc, st.Yc,
+				st.W, st.H,
+				st.Theta);
+		*/
+		pub.publish(st);
 	}
-	else if(theta<90)
-	{	int cont = -1*theta/90;
-		info.Theta = theta+cont*90;
+
+	/**
+	 * Calculate the euclidian distance between two points
+	 *
+	 *
+	 * @param x_1 coordiante x of first point
+	 * @param x_2 coordiante x of first point
+	 * @param x_3 coordiante x of first point
+	 * @param x_4 coordiante x of first point
+	 * @return norm euclidian dist of points
+	 */
+	float computeEucDist(const float x_1, const float y_1, const float x_2, const float y_2) 
+	{
+		float vector_x = x_2 - x_1;
+		float vector_y = y_2 - y_1;
+		return sqrt(abs(vector_x * vector_x) + abs(vector_y * vector_y));
 	}
-	
-	//Uncomment these lines to print the results on console
-	/*printf("Centroid in pix position (%f,%f)\n Object Width (%f)\n Object Height (%f)\n Theta (%f)\n", //Impresion en consola de los datos de posicion
-					
-			info.Xc, info.Yc,
-			info.W, info.H,
-			info.Theta);
-	*/
-	pub.publish(info);
 
-        
-  }
-
-
-};//End of class 
+	/**
+	 * Calculate the angle WRT X
+	 *
+	 *
+	 * @param x_1 coordiante x of first point
+	 * @param x_2 coordiante x of first point
+	 * @param x_3 coordiante x of first point
+	 * @param x_4 coordiante x of first point
+	 * @return th angle WRT X
+	 */
+	float computeTheta(const float x_1, const float y_1, const float x_2, const float y_2) 
+	{
+		float vector_x = x_2 - x_1;
+		float vector_y = y_2 - y_1;
+		return atan2(vector_y, vector_x) * 180 / M_PI;
+	}
+};
 
 
 int main(int argc, char** argv)
 {   
-
-    ros::init(argc, argv, "data_calculation"); //Nombre del nodo que uso para suscribirme y publicar la info
+    ros::init(argc, argv, "data_calculation"); // Node name
     ros::NodeHandle n;
-    Position detec(n);
-
-
+    States compt(n);
     ros::spin();
 
     return 0;
 }
+
