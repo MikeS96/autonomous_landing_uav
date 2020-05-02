eb39f6441f27d989a0978b1a307ed5d5bad5faf0
Sherlock <miguel.saaruiz@gmail.com>
2020-05-02 10:45:45 -0500

    commented header file for PID controller library

--- drone_controller/include/drone_controller/pid.h
+++ drone_controller/include/drone_controller/pid.h
@@ -1,31 +1,53 @@
+/**
+ *  @file pid.h
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief PID controller header files
+ *  @version 0.1
+ *  @date 05-01-2020
+ * 
+ *  Copyright (c) 2020 Miguel Saavedra
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+
 #ifndef PID_H
 #define PID_H
 
 class PID
 {
-
-
     private:
-	double max;
-	double min;
-	double kp;
-	double kd;
-	double ki;
-	double pre_error;
-	double integral;
-	double pre_integral;
-	
-    public:
-        // Kp -  proportional gain
-        // Ki -  Integral gain
-        // Kd -  derivative gain
-        // dt -  loop interval time
-        // max - maximum value of manipulated variable
-        // min - minimum value of manipulated variable
-        PID(double cmax, double cmin, double ckp, double ckd, double cki);
+        double max; // max - maximum output value
+        double min; // min - minimum output value
+        double kp; // Kp -  proportional gain
+        double kd; // Kd -  derivative gain
+        double ki; // Ki -  Integral gain
+        double pre_error; // Error at (t-1)
+        double integral; // Integral term
+        double pre_integral; // Integral term at (t-1)
 
-        // Returns the manipulated variable given a setpoint and current process value
+    public:
+        // Class constructor
+        PID(double cmax, double cmin, double ckp, double ckd, double cki);
+        // Compute PID output
         double calculate( double setpoint, double pv, double cdt);
+        // Class destructor
         ~PID();
 
     
